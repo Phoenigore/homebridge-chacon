542e967014fd760fdf3b272f9fcca4567159e0d5
Romain BAJEUX <romain@famille-bajeux.com>
2019-11-15 23:01:11 +0100

    First commit after Fork of homebridge-rf-outlets

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ### THIS PROJECT IS UNDER DEVELOPPEMENT - FORK NOT FINISHED !
 
-# homebridge-rfoutlets
+# homebridge-rfchacon
 
-Homebridge plugin purpose-built for controlling 433MHz radio frequency outlets which can be purchased inexpensively. (i.e. [these](https://www.amazon.com/Etekcity-Wireless-Electrical-Household-Appliances/dp/B00DQELHBS "Etekcity Wireless Outlets")). Homebridge maintains state information, so you can always know if you left that lamp on.
+Homebridge plugin purpose-built for controlling 433MHz radio frequency outlets, using chacon / dio protocol, which can be purchased inexpensively. (i.e. [these](https://www.amazon.com/Etekcity-Wireless-Electrical-Household-Appliances/dp/B00DQELHBS "Etekcity Wireless Outlets")). Homebridge maintains state information, so you can always know if you left that lamp on.
 
-Tested on a Raspberry Pi 2 - Model B running Raspbian (Debian) and Node.js v6.9.0. This should work on most Raspberry Pi models, however YMMV
+Tested on a Raspberry Pi Zero running Raspbian (Debian) and Node.js v6.9.0. This should work on most Raspberry Pi models, however YMMV
 
 Supports *lights*, *switches*, and *fans*
 
@@ -14,13 +14,13 @@ Supports *lights*, *switches*, and *fans*
 `sudo npm install -g homebridge`
 
 - Install homebridge-rfoutlets  
-`sudo npm install -g homebridge-rfoutlets`
+`sudo npm install -g homebridge-rfchacon` --Not Packaged Yet
 
 - Update your homebridge configuration
 
 ## Notes
 
-- The user which homebridge is run from must be on the list of *sudoers* as the `codesend` executable requires root privileges
+- The user which homebridge is run from must be on the list of *sudoers* as the `chacon_send` executable requires root privileges
 - A great guide on how to record RF signals and set up your Pi to transmit can be found [here](https://www.samkear.com/hardware/control-power-outlets-wirelessly-raspberry-pi "Pi 433Mhz Transmitter Guide"). Note: the web portion of the guide is not required
 
 ## Configuration
@@ -30,9 +30,7 @@ Supports *lights*, *switches*, and *fans*
 - `manufacturer`: manufacturer of the device plugged into the outlet (*optional*, defaults to *blank*)
 - `model`: model of the device plugged into the outlet (*optional*, defaults to *blank*)
 - `serial`: serial number of the device plugged into the outlet (*optional*, defaults to *blank*)
-- `rf_on`: RF signal to turn the outlet on (**required**)
-- `rf_off`: RF signal to turn the outlet off (**required**)
-- `pulselength`: RF transmission pulse length (*optional*, defaults to 189)
 - `pin`: GPIO pin of the 433MHz transmitter (*optional*, defaults to 0)
-
+- `module_id` : Remote control Id you want to emulate with 433Mhz sender. Could be randomize if your outlet has learning mode
+- `outlet_code` : Position button on your remote control. Could be randomize if your outlet has learning mode
 See `sample-config.json`
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var Service, Characteristic, limiter, cmdBase;
+var Service, Characteristic, limiter;
 var exec = require("child_process").exec,
     RateLimiter = require('limiter').RateLimiter;
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    limiter = new RateLimiter(1, 700); //limit requests to one per 200ms
-    homebridge.registerAccessory("homebridge-rfoutlets",
-        "RFOutlet",
+    limiter = new RateLimiter(1, 200); //limit requests to one per 200ms
+    homebridge.registerAccessory("homebridge-chacon",
+        "RFChacon",
         RFOutletAccessory);
 }
 
@@ -22,13 +22,11 @@ function RFOutletAccessory(log, config) {
     this.serial = config["serial"];
 
     //RF transmit inforamtion
-    this.rf_on = config["rf_on"];
-    this.rf_off = config["rf_off"];
-
-    if (config["pulselength"]) {
-        this.pulselength = config["pulselength"];
-    } else {
-        this.pulselength = 189; //Default to a pulse length of 189
+    this.rf_on = "on";
+    this.rf_off = "off";
+    if(config["inverted"] || false){
+        this.rf_on = "off";
+        this.rf_off = "on";
     }
 
     if (config["pin"]) {
@@ -37,9 +35,24 @@ function RFOutletAccessory(log, config) {
         this.pin = 0; //Default to GPIO pin 0
     }
 
-    cmdBase = "sudo " + //the codesend executable requires root
+    if(config["module_id"]){
+        this.module_id = config["module_id"];
+    } else{
+        this.module_id = "12325261" // Default Id, can be changed in config
+    }
+
+    if(config["outlet_code"]){
+        this.outlet_code = config["outlet_code"];
+    } else{
+	    this.outlet_code = "1"; //Default outlet code, can be change in config
+    }
+
+
+    this.cmdBase = "sudo " + //the codesend executable requires root
         __dirname + //module directory
-        "/codesend -p " + this.pin + " -l " + this.pulselength + " ";
+        "/chacon_send " + this.pin + " " + this.module_id + " " + this.outlet_code + " ";
+    var that = this;
+    that.setPowerState(false, function(){ }); //Set default state to false
 }
 
 RFOutletAccessory.prototype = {
@@ -48,19 +61,21 @@ RFOutletAccessory.prototype = {
         var cmd;
 
         if (powerOn) {
-            cmd = cmdBase + this.rf_on;
+            cmd = this.cmdBase + this.rf_on;
             state = "on";
         } else {
-            cmd = cmdBase + this.rf_off;
+            cmd = this.cmdBase + this.rf_off;
             state = "off";
         }
 
         this.log("Turning " + this.name + " " + state);
+        this.log(cmd);
 
-        limiter.removeTokens(1, function() {
+    limiter.removeTokens(1, function() {
+	    RFOutletAccessory.runningByAnotherItem = true;
             exec(cmd, function(error, stdout, stderr) {
                 if (error) {
-                    console.log(error);
+                        console.log(error);
                 }
                 callback();
             })
@@ -92,6 +107,9 @@ RFOutletAccessory.prototype = {
             case "Fan":
                 this.outletService = new Service.Fan(this.name);
                 break;
+	    case "Heater":
+		this.outletService = new Service.HeaterCooler(this.name);
+		break;
             default:
                 this.outletService = new Service.Switch(this.name);
         }
